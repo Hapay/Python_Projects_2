07394863fb5d558b9fd005776732fa629a447d64
Patryk <xhapay@gmail.com>
2020-05-07 01:06:33 +0200

    Using recursion statements czyli {} do szybieko obliczania https://realpython.com/python-thinking-recursively/ Tutaj jest link

---
+++ Fibonacci_list.py
@@ -0,0 +1,32 @@
+# Fibonacci works like this: 1 1 2 3 5 8 13 21 czyli bierze ostatnie dwie liczby i do siebie dodaje
+
+# Moja metoda, ale tylko do liczby 34!
+def fibo(num):
+    list = [1, 1]
+    i = 0
+    j = 1
+    k = list[i]
+    l = list[j]
+    for item in list:
+        if i != num and num < 35:
+            list.append(k + l)
+            i += 1
+            j += 1
+            k = list[i]
+            l = list[j]
+    return list[num - 1]
+#print(fibo(8))  # 34 to najwyzsza mozliwa
+
+
+# Using recursion statement
+def Fibonacci(n):
+    nums = {}
+    if n <= 2:
+        return 1
+    if n in nums:
+        return nums[n]
+    else:
+        num = Fibonacci(n - 1) + Fibonacci(n - 2)
+        nums[n] = num
+        return num
+print(Fibonacci())
